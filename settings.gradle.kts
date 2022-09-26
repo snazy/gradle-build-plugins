4c04058300fca59f5c590c0aa17888654c0c72e8
Robert Stupp <snazy@snazy.de>
2022-09-26 09:57:10 +0200

    Add renovate (#50)
    Adds a default revovate configuration with automerge enabled for
    non-major-version upgrades for managers that have CI coverage:
    maven, gradle, gradle-wrapper.
    
    Also works for more managers: github actions, etc.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,48 +19,22 @@ if (!JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_11)) {
 }
 
 pluginManagement {
+  val versionIdeaExtPlugin = "1.1.6"
+  val versionSpotlessPlugin = "6.11.0"
+
   // Cannot use a settings-script global variable/value, so pass the 'versions' Properties via
   // settings.extra around.
   val versions = java.util.Properties()
-  val pluginIdPattern =
-    java.util.regex.Pattern.compile("\\s*id\\(\"([^\"]+)\"\\) version \"([^\"]+)\"\\s*")
   settings.extra["nessieBuildTools.versions"] = versions
 
   plugins {
+    id("com.gradle.plugin-publish") version "1.0.0"
+    id("com.diffplug.spotless") version versionSpotlessPlugin
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("org.jetbrains.gradle.plugin.idea-ext") version versionIdeaExtPlugin
 
-    // Note: this is NOT a real project but a hack for dependabot to manage the plugin versions.
-    //
-    // Background: dependabot only manages dependencies (incl Gradle plugins) in build.gradle[.kts]
-    // files. It scans the root build.gradle[.kts] fila and those in submodules referenced in
-    // settings.gradle[.kts].
-    // But dependabot does not manage managed plugin dependencies in settings.gradle[.kts].
-    // However, since dependabot is a "dumb search and replace engine", we can use a trick:
-    // 1. Have this "dummy" build.gradle.kts file with all managed plugin dependencies.
-    // 2. Add an `include()` to this build file in settings.gradle.kts, surrounded with an
-    //    `if (false)`, so Gradle does _not_ pick it up.
-    // 3. Parse this file in our settings.gradle.kts, provide a `ResolutionStrategy` to the
-    //    plugin dependencies.
-
-    val pulledVersions =
-      file("gradle/dependabot/build.gradle.kts")
-        .readLines()
-        .map { line -> pluginIdPattern.matcher(line) }
-        .filter { matcher -> matcher.matches() }
-        .associate { matcher -> matcher.group(1) to matcher.group(2) }
-
-    resolutionStrategy {
-      eachPlugin {
-        if (requested.version == null) {
-          val pluginId = requested.id.id
-          if (pulledVersions.containsKey(pluginId)) {
-            useVersion(pulledVersions[pluginId])
-          }
-        }
-      }
-    }
-
-    versions["versionIdeaExtPlugin"] = pulledVersions["org.jetbrains.gradle.plugin.idea-ext"]
-    versions["versionSpotlessPlugin"] = pulledVersions["com.diffplug.spotless"]
+    versions["versionIdeaExtPlugin"] = versionIdeaExtPlugin
+    versions["versionSpotlessPlugin"] = versionSpotlessPlugin
   }
 
   repositories {
@@ -92,7 +66,3 @@ listOf(
     "dependency-declarations"
   )
   .forEach { include(it) }
-
-if (false) {
-  include("gradle:dependabot")
-}
